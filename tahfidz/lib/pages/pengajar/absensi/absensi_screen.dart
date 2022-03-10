e2b77373002a9d87a78901448961c9df94c210f9
naneps <ekaprasetya2244@gmail.com>
2022-03-10 09:14:37 +0700

    Try fix bug

--- tahfidz/lib/pages/pengajar/absensi/absensi_screen.dart
+++ tahfidz/lib/pages/pengajar/absensi/absensi_screen.dart
@@ -25,7 +25,10 @@ class _AbsensiScreenState extends State<AbsensiScreen> {
   });
 
   bool _isAccepted = false;
-  Color color1 = Colors.green;
+  Color hadir = Colors.green;
+  Color izin = Colors.blue;
+  Color sakit = Colors.yellow;
+  Color alpa = Colors.red;
   Color? targetC;
   @override
   Widget build(BuildContext context) {
@@ -48,223 +51,228 @@ class _AbsensiScreenState extends State<AbsensiScreen> {
       body: Container(
         // physics: BouncingScrollPhysics(),s
         height: heightBody,
-        // color: Colors.white,
-        child: Container(
-          height: heightBody,
-          width: widhtBody,
-          // padding: EdgeInsets.all(20),
-          // color: Colors.black26,
-          child: Column(
-            children: [
-              Container(
-                height: heightBody / 5,
-                width: widhtBody,
-                // color: Colors.black,
-                child: Stack(
-                  // fit: StackFit.expand,
-                  // clipBehavior: Clip.antiAliasWithSaveLayer,
-                  alignment: Alignment.center,
-                  children: [
-                    Positioned(
-                      top: 0,
-                      height: 100,
-                      width: widhtBody,
-                      child: Container(
-                        // color: mainColor,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(90),
-                              bottomRight: Radius.circular(90),
-                            ),
-                            color: mainColor),
-                        // width: widhtBody,
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      height: 150,
-                      width: widhtBody / 1.2,
-                      child: Card(
-                        // color: mainColor,
-                        elevation: 1,
-                        shadowColor: Colors.transparent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(30),
-                          ),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.all(20),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            // mainAxisSize: MainAxisSize.min,
+        // color: Colors.black,
 
-                            children: [
-                              Draggable(
-                                feedback: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: color1.withOpacity(0.5),
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                                data: color1,
-                                child: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: color1,
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                              ),
-                              Draggable(
-                                feedback: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: color1.withOpacity(0.5),
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                                data: color1,
-                                child: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: Colors.blue,
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                              ),
-                              Draggable(
-                                feedback: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: color1.withOpacity(0.5),
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                                data: color1,
-                                child: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: Colors.yellow,
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                              ),
-                              Draggable(
-                                feedback: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: color1.withOpacity(0.5),
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                                data: color1,
-                                child: SizedBox(
-                                  width: 40,
-                                  height: 40,
-                                  child: Material(
-                                    color: Colors.red,
-                                    shape: StadiumBorder(),
-                                    elevation: 4,
-                                  ),
-                                ),
-                              ),
-                            ],
+        child: Column(
+          children: [
+            Container(
+              height: heightBody / 5,
+              width: widhtBody,
+              // color: Colors.black,
+              child: Stack(
+                // fit: StackFit.expand,
+                // clipBehavior: Clip.antiAliasWithSaveLayer,
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    top: 0,
+                    height: 100,
+                    width: widhtBody,
+                    child: Container(
+                      // color: mainColor,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(90),
+                            bottomRight: Radius.circular(90),
                           ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: heightBody / 16,
-                width: widhtBody / 1.4,
-                // color: Colors.black,
-                child: Card(
-                  elevation: 5,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: heightBody / 2.2,
-                width: widhtBody,
-                // color: Colors.black,
-                padding: EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                ),
-                // color: Colors.green,
-                child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: users.length,
-                  itemBuilder: (context, index) => Card(
-                    elevation: 5,
-                    // shape: ,
-                    margin: EdgeInsets.all(10),
-                    child: ListTile(
-                      title: Text(
-                        "${users[index]['name']}",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w600),
-                      ),
-                      // contentPadding: EdgeInsets.all(5),
-                      trailing: InnerShadowBox(
-                        child: DragTarget(
-                          onAccept: (value) {
-                            _isAccepted = true;
-                            targetC = value as Color?;
-                          },
-                          builder: (context, candidate, rejected) {
-                            return (_isAccepted)
-                                ? SizedBox(
-                                    width: 30,
-                                    height: 30,
-                                    child: Material(
-                                      color: targetC,
-                                      shape: StadiumBorder(),
-                                      elevation: 4,
-                                    ),
-                                  )
-                                : Text('no');
-                          },
-                        ),
-                      ),
-
-                      leading: CircleAvatar(
-
-                          // child: Text("te"),
-                          ),
+                          color: mainColor),
+                      // width: widhtBody,
                     ),
                   ),
-                ),
+                  Positioned(
+                    top: 0,
+                    height: 150,
+                    width: widhtBody / 1.2,
+                    child: Card(
+                      // color: mainColor,
+                      elevation: 1,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          // mainAxisSize: MainAxisSize.min,
+
+                          children: [
+                            Draggable(
+                              feedback: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: hadir.withOpacity(0.5),
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                              data: hadir,
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: hadir,
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                            ),
+                            Draggable(
+                              feedback: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: izin.withOpacity(0.5),
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                              data: izin,
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: izin,
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                            ),
+                            Draggable(
+                              feedback: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: sakit.withOpacity(0.5),
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                              data: sakit,
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: Colors.yellow,
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                            ),
+                            Draggable(
+                              feedback: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: alpa.withOpacity(0.5),
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                              data: alpa,
+                              child: SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Material(
+                                  color: alpa,
+                                  shape: StadiumBorder(),
+                                  elevation: 4,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: heightBody / 16,
+              width: widhtBody / 1.4,
+              // color: Colors.black,
+              child: Card(
+                elevation: 5,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: heightBody / 2.2,
+              width: widhtBody,
+              // color: Colors.black,
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10,
+              ),
+              // color: Colors.green,
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index]['id'];
+                  return buildUser(index, user);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  Widget buildUser(int index, dynamic user) => Card(
+        key: ValueKey(user),
+        elevation: 2,
+        // shape: ShapeBorder,
+        margin: EdgeInsets.all(10),
+        child: ListTile(
+          key: ValueKey(index),
+          title: Text(
+            "${users[index]['name']}",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          // contentPadding: EdgeInsets.all(5),
+          trailing: InnerShadowBox(
+            child: DragTarget(
+              onAccept: (
+                value,
+              ) {
+                _isAccepted = true;
+                targetC = value as Color?;
+                // users[index];
+                print(user[index]['id']);
+              },
+              builder: (context, candidate, rejected) {
+                return (_isAccepted)
+                    ? SizedBox(
+                        width: 35,
+                        height: 35,
+                        child: Material(
+                          color: targetC,
+                          shape: StadiumBorder(),
+                          elevation: 1,
+                        ),
+                      )
+                    : Text('no');
+              },
+            ),
+          ),
+
+          leading: CircleAvatar(
+
+              // child: Text("te"),
+              ),
+        ),
+      );
 }
 
 class InnerShadowBox extends StatelessWidget {
@@ -293,7 +301,8 @@ class InnerShadowBox extends StatelessWidget {
         ],
         borderRadius: BorderRadius.circular(25),
       ),
-      child: Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 5, left: 5),
         child: child,
       ),
     );
