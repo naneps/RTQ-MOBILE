eed4dd07c247f9fe6326c0374be9167684120e00
naneps <ekaprasetya2244@gmail.com>
2022-03-15 00:13:36 +0700

    Modify Absensi Page

--- tahfidz/lib/pages/pengajar/absen_saya/my_absensi_page.dart
+++ tahfidz/lib/pages/pengajar/absen_saya/my_absensi_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,6 +17,7 @@ class MyAbsen extends StatefulWidget {
 
 class _MyAbsenState extends State<MyAbsen> {
   File? imageFile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,43 +36,89 @@ class _MyAbsenState extends State<MyAbsen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              margin: EdgeInsets.all(20),
-              width: 400,
-              height: 400,
-              color: mainColor,
-              child: (imageFile != null)
-                  ? Image.file(imageFile!)
-                  : SizedBox(
-                      width: 300,
-                      height: 300,
+              height: MediaQuery.of(context).size.height / 1.94,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(10),
+              clipBehavior: Clip.none,
+              // color: Colors.grey,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    top: 0,
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: mainColor,
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      width: 350,
+                      height: 350,
+                      child: (imageFile != null)
+                          ? Image.file(imageFile!)
+                          : SizedBox(
+                              width: 300,
+                              height: 300,
+                              child: Material(
+                                child: Image.asset('assets/images/guided.jpg',
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
                     ),
+                  ),
+                  Positioned(
+                      bottom: 0,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          imageFile = await Navigator.push<File>(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CameraScreen(),
+                            ),
+                          );
+                          setState(() {});
+                        },
+                        style: ElevatedButton.styleFrom(
+                          elevation: 5,
+                          primary: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(100),
+                            ),
+                          ),
+                        ),
+                        child: SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: Icon(
+                            Icons.camera,
+                            size: 40,
+                            color: mainColor,
+                          ),
+                        ),
+                      ))
+                ],
+              ),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                imageFile = await Navigator.push<File>(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CameraScreen(),
-                  ),
-                );
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(100),
-                  ),
-                ),
+            Container(
+              margin: EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width / 1.2,
+              height: MediaQuery.of(context).size.width / 7.5,
+              // color: mainColor,
+              child: TextField(
+                readOnly: true,
+                // focusNode: FocusNode(),
+                scrollPadding: EdgeInsets.all(10),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800], fontSize: 16),
+                    hintText: 'JL.Lohbener lama No 14',
+                    fillColor: Colors.white),
               ),
-              child: SizedBox(
-                height: 60,
-                width: 60,
-                child: Icon(
-                  Icons.camera,
-                  size: 40,
-                ),
-              ),
-            )
+            ),
           ],
         ),
       ),
