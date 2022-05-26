7eee181e2082d7d9bbc383b4b1b12cc387cc25cb
naneps <ekaprasetya2244@gmail.com>
2022-05-27 01:05:45 +0700

    udah yaa capee

--- tahfidz/test/widget_test.dart
+++ tahfidz/test/widget_test.dart
@@ -16,35 +16,35 @@ void main() async {
     String token =
         "c48ea743e989a545fd9673bc26f0bddbd0766738deefb9c349bbe56e5725ce227dd1085f56d2ae6b";
 
-    // Future<void> updateNilai(
-    //     {String? idNilai, String? idAsatidz, String? nilai}) async {
-    //   var body = {
-    //     'nilai': nilai,
-    //   };
-    //   var json = jsonEncode(body);
+    Future<void> updateNilai(
+        {String? idNilai, String? idAsatidz, String? nilai}) async {
+      var body = {
+        'nilai': nilai,
+      };
+      var json = jsonEncode(body);
 
-    //   try {
-    //     var url = Uri.parse('$baseUrl/penilaian/put/$idNilai/$idAsatidz');
-    //     var response = await http.put(url,
-    //         headers: {
-    //           HttpHeaders.authorizationHeader: token,
-    //           // "Content-Type": "application/json",
-    //           HttpHeaders.contentTypeHeader: "application/json"
-    //         },
-    //         body: json);
+      try {
+        var url = Uri.parse('$baseUrl/penilaian/put/$idNilai/$idAsatidz');
+        var response = await http.put(url,
+            headers: {
+              HttpHeaders.authorizationHeader: token,
+              // "Content-Type": "application/json",
+              HttpHeaders.contentTypeHeader: "application/json"
+            },
+            body: json);
 
-    //     if (kDebugMode) {
-    //       print("StatusCode update Nilai : ${response.statusCode}");
-    //     }
-    //     if (response.statusCode == 200) {
-    //       print("Update Nilai Berhasil");
-    //     } else {
-    //       print("Update Nilai Gagal");
-    //     }
-    //   } catch (e) {
-    //     print("Catc.h Create Nilai : $e");
-    //   }
-    // }
+        if (kDebugMode) {
+          print("StatusCode update Nilai : ${response.statusCode}");
+        }
+        if (response.statusCode == 200) {
+          print("Update Nilai Berhasil");
+        } else {
+          print("Update Nilai Gagal");
+        }
+      } catch (e) {
+        print("Catc.h Create Nilai : $e");
+      }
+    }
 
     // await updateNilai(idNilai: "2", idAsatidz: "1", nilai: "40");
   } on Exception catch (e) {
