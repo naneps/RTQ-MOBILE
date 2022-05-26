65f4a0456b4ebab186e0e382a8250585448f1554
naneps <ekaprasetya2244@gmail.com>
2022-05-26 14:59:50 +0700

    add const

--- tahfidz/lib/views/asatidz/penilaian/components/card_pelajaran.dart
+++ tahfidz/lib/views/asatidz/penilaian/components/card_pelajaran.dart
@@ -105,7 +105,7 @@ class _CardPelajaranState extends State<CardPelajaran> {
                     child: Text("Error"),
                   );
                 } else {
-                  return Center(
+                  return const Center(
                     child: Text("Nilai Kosong"),
                   );
                 }
