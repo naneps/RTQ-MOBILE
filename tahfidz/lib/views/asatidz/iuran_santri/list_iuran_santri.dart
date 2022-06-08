4af73e3ed2ad699a98f86e046d1c55d9c35d4bf8
naneps <ekaprasetya2244@gmail.com>
2022-06-08 22:11:39 +0700

    showing data iuran

--- tahfidz/lib/views/asatidz/iuran_santri/list_iuran_santri.dart
+++ tahfidz/lib/views/asatidz/iuran_santri/list_iuran_santri.dart
@@ -5,6 +5,8 @@ import 'package:sp_util/sp_util.dart';
 import 'package:tahfidz/components/constants.dart';
 import 'package:tahfidz/components/custom_text_field.dart';
 import 'package:tahfidz/controllers/iuran_controller.dart';
+import 'package:tahfidz/data/dumy+data.dart';
+import 'package:tahfidz/model/iuran.dart';
 import 'package:tahfidz/services/remote_services.dart';
 
 class ListIuranSantri extends StatelessWidget {
@@ -48,121 +50,162 @@ class ListIuranSantri extends StatelessWidget {
                 kdHalaqoh: argumen['kd_halaqoh'],
               ),
               builder: (context, AsyncSnapshot snapshot) {
-                return snapshot.hasData
-                    ? ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 15),
-                              // height: 50,
-                              width: Get.height,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: const [
-                                  BoxShadow(
-                                      color: Colors.grey,
-                                      blurRadius: 1,
-                                      spreadRadius: 1,
-                                      offset: Offset(1, 1))
-                                ],
-                              ),
-                              // color: Colors.amberAccent,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text(
-                                        "${snapshot.data[index].namaLengkap}",
-                                        style: GoogleFonts.poppins(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  // Text("${snapshot.data[index].namaLengkap}"),
-                                  TextButton(
-                                      onPressed: () {
-                                        Get.bottomSheet(
-                                          Container(
-                                            padding: EdgeInsets.all(15),
-                                            height: Get.height / 4,
-                                            color: Colors.white,
-                                            child: Column(
-                                              children: [
-                                                Text("Tambah Iuran"),
-                                                CustomTextField(
-                                                  hintText: "Nominal",
-                                                  labelText: "Nominal",
-                                                  controller: controller
-                                                      .iuranController,
-                                                ),
-                                                Row(
-                                                  children: [
-                                                    ElevatedButton(
-                                                        onPressed: () {
-                                                          Get.back();
-                                                        },
-                                                        child: Text("Batal")),
-                                                    ElevatedButton(
-                                                        onPressed: () async {
-                                                          await RemoteServices
-                                                              .storeIuran(
-                                                            idSantri: snapshot
-                                                                .data[index].id,
-                                                            nominal: controller
-                                                                .iuranController
-                                                                .text,
-                                                          ).then(
-                                                            (value) {
-                                                              print(value);
-                                                              if (value ==
-                                                                  true) {
-                                                                Get.back();
-                                                                Get.snackbar(
-                                                                    "Berhasil",
-                                                                    "Berhasil menambahkan iuran",
-                                                                    backgroundColor:
-                                                                        Colors
-                                                                            .green);
-                                                              }
-                                                            },
-                                                          );
-                                                        },
-                                                        child: Text("Simpan")),
-                                                  ],
-                                                )
-                                              ],
-                                            ),
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  return snapshot.hasData
+                      ? ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 15, vertical: 15),
+                                // height: 50,
+                                width: Get.height,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Colors.grey,
+                                        blurRadius: 1,
+                                        spreadRadius: 1,
+                                        offset: Offset(1, 1))
+                                  ],
+                                ),
+                                // color: Colors.amberAccent,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "${snapshot.data[index].namaLengkap}",
+                                          style: GoogleFonts.poppins(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w500,
                                           ),
-                                        );
-                                      },
-                                      child: Container(
-                                        child: Row(
-                                          children: [
-                                            Icon(Icons.add),
-                                            Text("Tambah Iuran")
-                                          ],
                                         ),
-                                      ))
-                                  // Text("${snapshot.data[index].namaLengkap}"),
-                                ],
-                              ));
-                        },
-                      )
-                    : Center(
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation(
-                            kMainColor,
+                                      ],
+                                    ),
+                                    // Text("${snapshot.data[index].namaLengkap}"),
+                                    FutureBuilder(
+                                      future: RemoteServices.getNominalIuran(
+                                        snapshot.data[index].id.toString(),
+                                      ),
+                                      builder: (context,
+                                          AsyncSnapshot snapshotIuran) {
+                                        // print(snapshotIuran.data['nominal']);
+                                        if (snapshotIuran.connectionState ==
+                                            ConnectionState.waiting) {
+                                          return const Center(
+                                            child: CircularProgressIndicator(
+                                              valueColor:
+                                                  AlwaysStoppedAnimation(
+                                                      Color.fromARGB(
+                                                          255, 96, 33, 243)),
+                                            ),
+                                          );
+                                        } else if (snapshotIuran.hasData) {
+                                          return Text(
+                                            formatRupiah(
+                                              int.parse(
+                                                snapshotIuran.data['nominal'],
+                                                // "1000000",
+                                              ),
+                                            ),
+                                          );
+                                        } else if (!snapshotIuran.hasData) {
+                                          return TextButton(
+                                              onPressed: () {
+                                                showInputIuran(
+                                                    idSantri:
+                                                        snapshot.data[index].id,
+                                                    nominal: controller
+                                                        .iuranController.text,
+                                                    controller: controller
+                                                        .iuranController);
+                                              },
+                                              child: Container(
+                                                child: Row(
+                                                  children: [
+                                                    Icon(Icons.add),
+                                                    Text("Tambah Iuran")
+                                                  ],
+                                                ),
+                                              ));
+                                        } else {
+                                          return Container();
+                                        }
+                                        // return Container();
+                                      },
+                                    )
+                                    // Text("${snapshot.data[index].namaLengkap}"),
+                                  ],
+                                ));
+                          },
+                        )
+                      : Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation(
+                              kMainColor,
+                            ),
                           ),
-                        ),
-                      );
+                        );
+                }
               },
             )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showInputIuran(
+      {String? idSantri, String? nominal, TextEditingController? controller}) {
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.all(15),
+        height: Get.height / 4,
+        color: Colors.white,
+        child: Column(
+          children: [
+            const Text("Tambah Iuran"),
+            CustomTextField(
+              hintText: "Nominal",
+              labelText: "Nominal",
+              controller: controller,
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text("Batal")),
+                ElevatedButton(
+                  onPressed: () async {
+                    await RemoteServices.storeIuran(
+                      idSantri: idSantri,
+                      nominal: nominal,
+                    ).then(
+                      (value) {
+                        print(value);
+                        if (value == true) {
+                          Get.back();
+                          Get.snackbar("Berhasil", "Berhasil menambahkan iuran",
+                              backgroundColor: Colors.green);
+                        }
+                      },
+                    );
+                  },
+                  child: const Text("Simpan"),
+                ),
+              ],
+            )
           ],
         ),
       ),
