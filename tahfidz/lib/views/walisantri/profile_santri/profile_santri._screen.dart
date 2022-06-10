38e212748ce9cbb22de05f75065d9889e8448f81
naneps <ekaprasetya2244@gmail.com>
2022-06-10 09:14:48 +0700

    revisi profile santri

--- tahfidz/lib/views/walisantri/profile_santri/profile_santri._screen.dart
+++ tahfidz/lib/views/walisantri/profile_santri/profile_santri._screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tahfidz/components/constants.dart';
 import 'package:tahfidz/services/remote_services.dart';
 import 'package:tahfidz/views/walisantri/profile_santri/components/card_santri.dart';
@@ -11,16 +12,23 @@ class ProfileAnakScreen extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profile Santri"),
-        backgroundColor: mainColor,
+        title: Text(
+          "Profile Santri",
+          style: GoogleFonts.poppins(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: kFontColor,
+          ),
+        ),
+        backgroundColor: kBackground,
         centerTitle: true,
         // excludeHeaderSemantics: true,
         elevation: 0,
       ),
-      backgroundColor: mainColor,
+      backgroundColor: kBackground,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 15, right: 15, left: 15),
+          padding: const EdgeInsets.only(top: 10, right: 15, left: 15),
           child: Container(
               padding: const EdgeInsets.all(10),
               width: size.width,
