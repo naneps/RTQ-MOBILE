3331bc706bf2a861f4f7b21c86802f0814166e07
naneps <ekaprasetya2244@gmail.com>
2022-06-23 01:40:06 +0700

    lelah

--- tahfidz/test/widget_test.dart
+++ tahfidz/test/widget_test.dart
@@ -7,6 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:sp_util/sp_util.dart';
 import 'package:tahfidz/data/helper.dart';
 import 'package:tahfidz/model/santri_by.dart';
 import 'package:tahfidz/services/remote_services.dart';
@@ -18,20 +19,15 @@ void main() async {
         "70985cb0c3eae7bb0d04ee33cc8d1aa29cc2dd8e1daf1836e17b6416b1a27cbf6c4b2b6d43b4068f";
     // String id = "1";
 
-    Future getAbsenTodaySantri(String id) async {
-      var absen = dataAbsenSantri.where((item) {
-        return item["tanggal"].day == DateTime.now().day;
+    Future cekAbsensiSantri({String? idJenjang, String? kdHalaqoh}) async {
+      var url = Uri.parse('$baseUrl/absensi/santri/$idJenjang/$kdHalaqoh');
+      var response = await http.get(url, headers: {
+        HttpHeaders.authorizationHeader: SpUtil.getString('token')!
       });
-      var data;
-      for (var item in absen) {
-        if (item["id_santri"] == id) {
-          data = item;
-        } else {
-          data = null;
-        }
-      }
 
-      return jsonEncode(data);
+      if (response.statusCode == 200) {
+        return 'true';
+      }
     }
 
     print(await getAbsenTodaySantri("2"));
